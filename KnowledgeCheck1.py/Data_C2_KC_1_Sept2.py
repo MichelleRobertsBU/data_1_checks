d47a565c74745eb79bfb004db9f0c86de8e00752
Michelle Roberts <80293351+MichelleRobertsBU@users.noreply.github.com>
2023-02-19 09:35:40 -0500

    Final push

--- KnowledgeCheck1.py/Data_C2_KC_1_Sept2.py
+++ KnowledgeCheck1.py/Data_C2_KC_1_Sept2.py
@@ -8,8 +8,10 @@ r = re.get(url)
 r_json = r.json()
 df = pd.json_normalize(data = r_json)
 print(df)
+print('')
 
-#Create larger dataframe, choose columns
+#Create larger dataframe, choose columns.
+
 beer_list = pd.DataFrame()
 url = "https://api.punkapi.com/v2/beers?"
 r = re.get(url)
@@ -18,6 +20,14 @@ df = pd.json_normalize(r_json)
 selection = df[["name", "abv", "tagline",]]
 beer_list = pd.concat([beer_list, selection])
 print(beer_list)
+print('')
+
+#Write a query in Pandas to select a particular set of your data.
+
+beer_abv = beer_list.query("abv > 5.0")
+print(beer_abv)
+print('')
+
 
 #Determine beer with lowest and highest abv (alcohol by volume)
 
@@ -28,10 +38,14 @@ df = pd.json_normalize(r_json)
 df_abv = df[["name","abv"]]
 df_low = df_abv["abv"].min()
 df_high = df_abv["abv"].max()
+print('')
 print("The beer with the lowest alcohol by volume is")
 print(df_abv.loc[df_abv["abv"]==df_abv["abv"].min(),:])
+print('')
 print("The beer with the highest alcohol by volume is")
 print(df_abv.loc[df_abv["abv"]==df_abv["abv"].max(),:])
+print('')
+
 
 #Select and print the SECOND AND THIRD columns of your data frame
 
@@ -40,7 +54,9 @@ r = re.get(url)
 r_json = r.json()
 df_large = pd.json_normalize(data = r_json)
 print(df_large.keys())
+print('')
 print(df_large[["name", "tagline"]])
+print('')
 
 #Select and print the FIRST 4 rows of your data frame
 print(df_large.iloc[:4])
